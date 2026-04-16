ab3d5cbef29712f7006dd0965a757d84a1f322fe
shahad-FS <alsaneea.s@gmail.com>
2026-04-16 06:11:59 +0000

    Create printTeacher function

--- TypeScript/task_1/js/main.ts
+++ TypeScript/task_1/js/main.ts
@@ -11,3 +11,8 @@ interface Teacher {
 interface Directors extends Teacher {
   numberOfReports: number;
 }
+
+
+function printTeacher(firstName: string, lastName: string): string {
+  return `${firstName[0]}. ${lastName}`;
+}
