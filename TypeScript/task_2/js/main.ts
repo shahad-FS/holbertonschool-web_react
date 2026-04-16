ef3d9777937b8d69f43a771c05a909bc1f1a839e
shahad-FS <alsaneea.s@gmail.com>
2026-04-16 06:53:19 +0000

    task_2 folder

---
+++ TypeScript/task_2/js/main.ts
@@ -0,0 +1,46 @@
+interface DirectorInterface {
+  workFromHome(): string;
+  getCoffeeBreak(): string;
+  workDirectorTasks(): string;
+}
+
+interface TeacherInterface {
+  workFromHome(): string;
+  getCoffeeBreak(): string;
+  workTeacherTasks(): string;
+}
+
+class Director implements DirectorInterface {
+  workFromHome(): string {
+    console.log("Working from home");
+    return "Working from home";
+  }
+
+  getCoffeeBreak(): string {
+    console.log("Getting a coffee break");
+    return "Getting a coffee break";
+  }
+
+  workDirectorTasks(): string {
+    console.log("Getting to director tasks");
+    return "Getting to director tasks";
+  }
+}
+
+class Teacher implements TeacherInterface {
+  workFromHome(): string {
+    return "Cannot work from home";
+  }
+  getCoffeeBreak(): string {
+    return "Cannot have a break";
+  }
+  workTeacherTasks(): string {
+    return "Getting to work";
+  }
+}
+
+function createEmployee(salary: number | string): Teacher | Director {
+  if (typeof salary === "number" && salary < 500) return new Teacher();
+
+  return new Director();
+}
